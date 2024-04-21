5d4766608904d472e77990cecac018760381b014
MChandraR <2201020103@student.umrah.ac.id>
2024-04-21 17:12:07 +0700

    Update job_finder.sql

--- database/job_finder.sql
+++ database/job_finder.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 13, 2024 at 06:09 PM
+-- Generation Time: Apr 21, 2024 at 12:11 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -34,22 +34,17 @@ CREATE TABLE `jobs` (
   `job_type_id` int(11) DEFAULT NULL,
   `description` text DEFAULT NULL,
   `begin` datetime DEFAULT NULL,
-  `end` datetime DEFAULT NULL
+  `end` datetime DEFAULT NULL,
+  `status` enum('open','closed','','') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `jobs`
 --
 
-INSERT INTO `jobs` (`job_id`, `job_name`, `employer_id`, `job_type_id`, `description`, `begin`, `end`) VALUES
-('J000000001', 'Web Developer with CI', 'U000000001', 1, 'Minimal punya laptop', '2024-04-14 00:00:00', '2024-05-13 00:00:00'),
-('J000000002', 'Web Developer with Laravel', 'U000000001', 1, 'Minimal punya laptop', '2024-04-14 00:00:00', '2024-05-13 00:00:00'),
-('J000000003', 'Mobile Developer : Android', 'U000000001', 1, 'Minimal punya laptop', '2024-04-14 00:00:00', '2024-05-13 00:00:00'),
-('J000000004', 'Mobile Developer : Flutter', 'U000000001', 1, 'Minimal punya laptop', '2024-04-14 00:00:00', '2024-05-13 00:00:00'),
-('U000000011', 'Dev.OPS', 'U000000001', 1, 'Kerja yang betul', '2024-04-24 00:00:00', '2024-04-25 00:00:00'),
-('U000000012', '123', 'U000000001', 1, 'Kerja yang betul', '2024-04-11 00:00:00', '2024-04-26 00:00:00'),
-('U000000013', 'Dev.OPS', 'U000000001', 1, 'Kerja yang betul', '2024-04-10 00:00:00', '2024-04-26 00:00:00'),
-('U000000014', 'It Solution', 'U000000001', 1, 'Kerja yang betul', '2024-04-17 00:00:00', '2024-04-25 00:00:00');
+INSERT INTO `jobs` (`job_id`, `job_name`, `employer_id`, `job_type_id`, `description`, `begin`, `end`, `status`) VALUES
+('J000000001', 'Dev.OPS', 'U000000001', 1, 'Kerja yang betul', '2024-04-08 00:00:00', '2024-04-29 00:00:00', 'open'),
+('J000000002', 'IOS Dev with Swift', 'U000000001', 2, 'Mobile developer berbasis IOS', '2024-04-17 00:00:00', '2024-04-12 00:00:00', 'open');
 
 -- --------------------------------------------------------
 
@@ -61,10 +56,20 @@ CREATE TABLE `job_appeal` (
   `appeal_id` varchar(10) NOT NULL,
   `job_id` varchar(10) DEFAULT NULL,
   `applicant_id` varchar(10) DEFAULT NULL,
+  `name` varchar(150) NOT NULL,
   `phone` varchar(13) DEFAULT NULL,
   `email` varchar(50) DEFAULT NULL,
-  `description` text DEFAULT NULL
+  `time` datetime NOT NULL DEFAULT current_timestamp(),
+  `description` text DEFAULT NULL,
+  `status` enum('pending','diterima','ditolak','') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `job_appeal`
+--
+
+INSERT INTO `job_appeal` (`appeal_id`, `job_id`, `applicant_id`, `name`, `phone`, `email`, `time`, `description`, `status`) VALUES
+('A000000001', 'J000000002', 'U000000001', 'Daffa', '083161366524', 'mcrcool04@gmail.com', '2024-04-21 13:28:08', '123', 'diterima');
 
 -- --------------------------------------------------------
 
@@ -83,7 +88,8 @@ CREATE TABLE `job_type` (
 --
 
 INSERT INTO `job_type` (`type_id`, `type_name`, `type_description`) VALUES
-(1, 'SOFTWARE DEVELOPER', 'Itu aja');
+(1, 'SOFTWARE DEVELOPER', 'Itu aja'),
+(2, 'SOFTWARE ENGINEER', 'Pengembang perangkat lunak');
 
 -- --------------------------------------------------------
 
@@ -131,12 +137,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`user_id`, `username`, `password`, `email`, `role`) VALUES
-('U000000001', 'users', 'users', 'users@gmail.com', 'user'),
-('U000000002', 'users', 'users', 'users@gmail.com', 'user'),
-('U000000003', '123', '123', 'mcrcool04@gmail.com', 'user'),
-('U000000004', 'user', 'user', 'users', 'user'),
-('U000000005', 'user', 'user', 'users', 'user'),
-('U000000006', 'mcr', '123', 'mcrcool04@gmail.com', 'user');
+('U000000001', 'users', 'users', 'users', 'user'),
+('U000000002', 'admin', 'admin', 'admin', 'user');
 
 --
 -- Indexes for dumped tables
@@ -186,7 +188,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `job_type`
 --
 ALTER TABLE `job_type`
-  MODIFY `type_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `type_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `migrations`
